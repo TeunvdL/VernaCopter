213827240f3bc6544e5da014517ab369ea36ca6c
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2023-11-15 12:18:19 +0100

    Add pipeline files

---
+++ Pipeline/NL_to_LTL.py
@@ -0,0 +1,5 @@
+"""
+This module provides functionality to transform a user-defined natural language specification
+into a linear temporal logic (LTL)-specification. The main function is `transform_to_ltl`, which
+takes a natural language string as input and returns the corresponding LTL formula as a string.
+"""
