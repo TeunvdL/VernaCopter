38953c89003669c8ef27a950698f6574c67176e9
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2023-11-15 12:18:42 +0100

    Write test for ChatGPT API

---
+++ Testing/openai-test.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
