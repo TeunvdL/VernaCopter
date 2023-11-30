10b4d601953535960b1f0cf3f72eb3371e85936c
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2023-11-30 16:00:25 +0100

    Add docstring

--- simple_pipeline/ChatGPT_move_to.py
+++ simple_pipeline/ChatGPT_move_to.py
@@ -1,10 +1,31 @@
 from openai import OpenAI
 
 class ChatGPT_move_to:
+    """
+    ChatGPT_move_to class for ChatGPT interaction.
+    
+    """
     def __init__(self):
         self.client = OpenAI()
 
     def chatcompletion(self, init_messages, user_input, model="gpt-3.5-turbo"):
+        """
+        ChatGPT interaction.
+
+        Parameters
+        ----------
+        init_messages : list of dict
+            Initial messages for ChatGPT.
+        user_input : list of dict
+            User input to get target position.
+        model : str, optional
+            ChatGPT model. The default is "gpt-3.5-turbo".
+
+        Returns
+        -------
+        str
+            Target position.
+        """
 
         messages = []
 
