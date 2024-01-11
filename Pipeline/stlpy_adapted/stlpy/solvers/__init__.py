3f4333d7eecf0e0edd7bd90308372be1dc8b27d3
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2024-01-11 14:31:17 +0100

    Remove redundant functionalities

--- Pipeline/stlpy_adapted/stlpy/solvers/__init__.py
+++ Pipeline/stlpy_adapted/stlpy/solvers/__init__.py
@@ -1,22 +1,4 @@
 # Test whether various optional dependencies are installed
-DRAKE_ENABLED = True
-try:
-    import pydrake
-except ImportError:
-    print("WARNING: pydrake import failed, Drake-based solvers disabled.")
-    print("         Install drake (https://drake.mit.edu/installation.html)")
-    print("         to use the Drake-based solvers.")
-    DRAKE_ENABLED = False
-
-SCIPY_ENABLED = True
-try:
-    import scipy
-except ImportError:
-    print("WARNING: scipy import failed, scipy-based solvers disabled.")
-    print("         Install scipy (https://scipy.org/install/)")
-    print("         to use the scipy-based solvers.")
-    SCIPY_ENABLED = False
-
 GUROBI_ENABLED = True
 try:
     import gurobipy
@@ -27,14 +9,5 @@ except ImportError:
     print("         are available.")
     GUROBI_ENABLED = False
 
-# And load the corresponding solvers accordingly
-if SCIPY_ENABLED:
-    from .scipy.gradient_solver import ScipyGradientSolver
-
 if GUROBI_ENABLED:
     from .gurobi.gurobi_micp import GurobiMICPSolver
-
-if DRAKE_ENABLED:
-    from .drake.drake_micp import DrakeMICPSolver
-    from .drake.drake_smooth import DrakeSmoothSolver
-    from .drake.drake_sos1 import DrakeSos1Solver
