2a66f2f9c5367f19597e4103e5028912d28cfa09
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2024-02-05 09:59:15 +0100

    Add distances plot

--- Pipeline_TL/main.py
+++ Pipeline_TL/main.py
@@ -3,19 +3,19 @@ from GPT import *
 from NL_to_STL import *
 from visualization import *
 
-T = 10
-max_acc = 10
+T = 10          # time horizon in seconds 
+max_acc = 10    # maximum acceleration in m/s^2
 
 objects = {"goal1": (4, 6, 4, 6, 4, 6),
            "goal2": (-5, -4, -5, -4, -5, -4),
            "goal3": (-6, -5, 4, 5, 4, 5),
            "obstacle1": (-1.5, -0.5, -1.5, -0.5, -1.5, -0.5),
            "obstacle2": (1.0, 2.0, 1.0, 2.0, 1.5, 2.5),
-           "obstacle3": (1.0, 2.0, 0.0, 1.0, -1.0, 1.0)}
+           "obstacle3": (1.0, 2.0, 0.0, 1.0, -1.0, 1.0),}
 
-x0 = np.array([0.,0.,0.,0.,0.,0.])
+x0 = np.array([0.,0.,0.,0.,0.,0.]) # initial state: x, y, z, vx, vy, vz
 
-user_input = [{"role": "user", "content": "Go to goal 1, 2 and 3. Avoid all obstacles."}]
+user_input = [{"role": "user", "content": "Go to goal 1 and 3. Avoid all obstacles."}]
 
 
 translator = NL_to_STL()
@@ -29,7 +29,14 @@ print("x: ", x)
 
 print("u: ", u)
 
-visualizer = Visualization(x, objects)
-#visualizer.visualize()
-gif_name = "basic_test.gif"
-visualizer.animate_trajectory(gif_name)
+animate = False
+visualizer = Visualization(x, objects, animate=animate)
+
+if animate:
+    gif_name = input("Enter name of GIF file: ")
+    visualizer.animate_trajectory(gif_name + ".gif")
+
+visualizer.visualize()
+visualizer.plot_distance_to_objects()
+
+plt.show()
