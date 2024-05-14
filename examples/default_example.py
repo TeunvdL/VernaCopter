5c722b7607f5f989433ced316670b277d58e718e
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2024-05-14 15:05:55 +0200

    Expand examples

--- examples/default_example.py
+++ examples/default_example.py
@@ -1,5 +1,17 @@
 from basics.setup import Default_parameters
 from main import main
+from experiments.save_results import save_results
+from basics.logger import color_text
 
-pars = Default_parameters() # Get the parameters
-main(pars)
+scenario_name = "treasure_hunt"                             # "reach_avoid", "narrow_maze", or "treasure_hunt"
+pars = Default_parameters(scenario_name = scenario_name)    # Get the parameters
+pars.save_results = True                                    # Save the results to a file
+
+try:
+    messages, task_accomplished = main(pars)                # Run the experiment
+except Exception as e:
+    print(e)
+    task_accomplished = False
+
+if pars.save_results:
+        save_results(pars, messages, task_accomplished)
--- examples/one_shot_automatic.py
+++ examples/one_shot_automatic.py
@@ -1,5 +1,21 @@
 from basics.setup import One_shot_parameters
 from main import main
+from experiments.save_results import save_results
+from basics.logger import color_text
 
-pars = One_shot_parameters() # Get the parameters
-main(pars)
+scenario_name = "treasure_hunt" # "reach_avoid", "narrow_maze", or "treasure_hunt"
+pars = One_shot_parameters(scenario_name = scenario_name) # Get the parameters
+
+N_experiments = 1
+
+for i in range(N_experiments):
+    print(color_text(f"Experiment {i+1}/{N_experiments}", "yellow"))
+    try:
+        messages, task_accomplished = main(pars) # Run the experiment
+    except Exception as e:
+        print(e)
+        task_accomplished = False
+        messages = []
+
+    if pars.save_results:
+            save_results(pars, messages, task_accomplished)
