c7513cc692ac061b3c55bca20e5c75912d0b8ef0
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2024-06-03 10:11:29 +0200

    Include more flags for experiments

--- examples/default_example.py
+++ examples/default_example.py
@@ -1,17 +1,21 @@
 from basics.setup import Default_parameters
 from main import main
 from experiments.save_results import save_results
-from basics.logger import color_text
 
-scenario_name = "treasure_hunt"                             # "reach_avoid", "narrow_maze", or "treasure_hunt"
+scenario_name = "reach_avoid"                               # "reach_avoid", "narrow_maze", or "treasure_hunt"
 pars = Default_parameters(scenario_name = scenario_name)    # Get the parameters
-pars.save_results = True                                    # Save the results to a file
+pars.save_results = False                                   # Save the results to a file
+pars.GPT_model = "gpt-4o"                                   # GPT model to use ("gpt-4o", "gpt-3.5-turbo")
+pars.spec_checker_enabled = False                           # Enable the specification checker
+pars.syntax_checker_enabled = False                         # Enable the syntax checker
+pars.animate_final_trajectory = True                        # Animate the final trajectory
 
 try:
     messages, task_accomplished = main(pars)                # Run the experiment
 except Exception as e:
     print(e)
     task_accomplished = False
+    messages = []
 
 if pars.save_results:
         save_results(pars, messages, task_accomplished)
