fdfe09311eee4063d6f8b7b3d82c56f2403d2d19
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2024-06-03 10:11:41 +0200

    Include more flags for experiments

--- examples/one_shot_automatic.py
+++ examples/one_shot_automatic.py
@@ -3,10 +3,13 @@ from main import main
 from experiments.save_results import save_results
 from basics.logger import color_text
 
-scenario_name = "treasure_hunt" # "reach_avoid", "narrow_maze", or "treasure_hunt"
+scenario_name = "reach_avoid" # "treasure_hunt", or "treasure_hunt"
 pars = One_shot_parameters(scenario_name = scenario_name) # Get the parameters
+pars.GPT_model = "gpt-4o" # "gpt-4o", "gpt-3.5-turbo"
+pars.spec_checker_enabled = True
+pars.syntax_checker_enabled = True
 
-N_experiments = 1
+N_experiments = 3 # Number of experiments to run
 
 for i in range(N_experiments):
     print(color_text(f"Experiment {i+1}/{N_experiments}", "yellow"))
