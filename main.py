0735d443283083b1d541ceb9cea45b028b22b5ef
Teun van de Laar <t.a.v.d.laar@student.tue.nl>
2024-12-06 11:20:15 +0100

    Prune redundant elements

--- main.py
+++ main.py
@@ -27,13 +27,16 @@ def main(pars=Default_parameters()):
     spec_checker_iteration = 0                  # Initialize the specification check iteration
     syntax_checker_iteration = 0                # Initialize the syntax check iteration
 
-    translator = NL_to_STL(scenario.objects, N, pars.dt, print_instructions=pars.print_ChatGPT_instructions, GPT_model = pars.GPT_model)
+    translator = NL_to_STL(scenario.objects, 
+                           N, 
+                           pars.dt, 
+                           print_instructions=pars.print_ChatGPT_instructions, 
+                           GPT_model = pars.GPT_model,)
 
     ### Main loop ###
     while status == "active":
 
         # Initialize/reset the flags
-        spec_accepted = False
         trajectory_accepted = False
 
         # Get the specification
@@ -42,7 +45,7 @@ def main(pars=Default_parameters()):
                                                            previous_messages=previous_messages, 
                                                            processing_feedback=processing_feedback, 
                                                            status=status, automated_user=pars.automated_user, 
-                                                           automated_user_input=scenario.automated_user_input)
+                                                           automated_user_input=scenario.automated_user_input,)
             
             if status == "exited": # If the user exits the conversation, break the loop
                 break
@@ -55,65 +58,64 @@ def main(pars=Default_parameters()):
             syntax_checked_spec = None
         print("Extracted specification: ", spec)
 
-        solver = STLSolver(spec, scenario.objects, x0, T) # Initialize the solver
+        solver = STLSolver(spec, scenario.objects, x0, T,)
 
-        if not pars.dynamicless_check_enabled or spec_accepted:
-            print(color_text("Generating the trajectory...", 'yellow'))
-            try:
-                x,u = solver.generate_trajectory(pars.dt, pars.max_acc, pars.max_speed, verbose=pars.solver_verbose, include_dynamics=True)
-                spec_checker = Spec_checker(scenario.objects, x, N, pars.dt)
-                inside_objects_array = spec_checker.get_inside_objects_array()
-                visualizer = Visualizer(x, scenario, animate=False)
-                fig, ax = visualizer.visualize_trajectory()
-                plt.pause(1)
-                fig, ax = spec_checker.visualize_spec(inside_objects_array)
-                plt.pause(1)
-                if pars.spec_checker_enabled and spec_checker_iteration < pars.spec_check_limit:
-                    spec_check_response = spec_checker.GPT_spec_check(scenario.objects, inside_objects_array, messages)
-                    trajectory_accepted = spec_checker.spec_accepted_check(spec_check_response)
-                    if not trajectory_accepted:
-                        print(color_text("The trajectory is rejected by the checker.", 'yellow'))
-                        spec_checker_message = {"role": "system", "content": f"Specification checker: {spec_check_response}"}
-                        messages.append(spec_checker_message)
-                        processing_feedback = True
-                    spec_checker_iteration += 1
-                elif spec_checker_iteration > pars.spec_check_limit:
-                    print(color_text("The program is terminated.", 'yellow'), "Exceeded the maximum number of spec check iterations.")
-                    break
+        print(color_text("Generating the trajectory...", 'yellow'))
+        try:
+            x,u = solver.generate_trajectory(pars.dt, pars.max_acc, pars.max_speed, verbose=pars.solver_verbose, include_dynamics=True)
+            spec_checker = Spec_checker(scenario.objects, x, N, pars.dt)
+            inside_objects_array = spec_checker.get_inside_objects_array()
+            visualizer = Visualizer(x, scenario, animate=False)
+            fig, ax = visualizer.visualize_trajectory()
+            plt.pause(1)
+            fig, ax = spec_checker.visualize_spec(inside_objects_array)
+            plt.pause(1)
+            if pars.spec_checker_enabled and spec_checker_iteration < pars.spec_check_limit:
+                spec_check_response = spec_checker.GPT_spec_check(scenario.objects, inside_objects_array, messages)
+                trajectory_accepted = spec_checker.spec_accepted_check(spec_check_response)
+                if not trajectory_accepted:
+                    print(color_text("The trajectory is rejected by the checker.", 'yellow'))
+                    spec_checker_message = {"role": "system", "content": f"Specification checker: {spec_check_response}"}
+                    messages.append(spec_checker_message)
+                    processing_feedback = True
+                spec_checker_iteration += 1
+            elif spec_checker_iteration > pars.spec_check_limit:
+                print(color_text("The program is terminated.", 'yellow'), "Exceeded the maximum number of spec check iterations.")
+                break
 
-                if np.isnan(x).all():
-                    raise Exception("The trajectory is infeasible.")
-            
-                if pars.manual_trajectory_check_enabled:
-                    # Ask the user to accept or reject the trajectory
-                    while True:
-                        response = input("Accept the trajectory? (y/n): ")
-                        if response.lower() == 'y':
-                            print(color_text("The trajectory is accepted.", 'yellow'))
-                            trajectory_accepted = True
-                            break  # Exit the loop since the trajectory is accepted
-                        elif response.lower() == 'n':
-                            print(color_text("The trajectory is rejected.", 'yellow'))
-                            trajectory_accepted = False
-                            break  # Exit the loop since the trajectory is rejected
-                        else:
-                            print("Invalid input. Please enter 'y' or 'n'.")
+            if np.isnan(x).all():
+                raise Exception("The trajectory is infeasible.")
+        
+            if pars.manual_trajectory_check_enabled:
+                # Ask the user to accept or reject the trajectory
+                while True:
+                    response = input("Accept the trajectory? (y/n): ")
+                    if response.lower() == 'y':
+                        print(color_text("The trajectory is accepted.", 'yellow'))
+                        trajectory_accepted = True
+                        break  # Exit the loop since the trajectory is accepted
+                    elif response.lower() == 'n':
+                        print(color_text("The trajectory is rejected.", 'yellow'))
+                        trajectory_accepted = False
+                        break  # Exit the loop since the trajectory is rejected
+                    else:
+                        print("Invalid input. Please enter 'y' or 'n'.")
 
-                if trajectory_accepted:
-                    all_x = np.hstack((all_x, x[:,1:]))
-                    x0 = x[:, -1]
-                    print("New position after trajectory: ", x0)
+            if trajectory_accepted:
+                all_x = np.hstack((all_x, x[:,1:]))
+                x0 = x[:, -1]
+                print("New position after trajectory: ", x0)
 
-            except:
-                print(color_text("The trajectory is infeasible.", 'yellow'))
-                if pars.syntax_checker_enabled and syntax_checker_iteration <= pars.syntax_check_limit:
-                    print(color_text("Checking the syntax of the specification...", 'yellow'))
-                    syntax_checked_spec = translator.gpt_syntax_checker(spec)
-                    syntax_checker_iteration += 1
+        except:                                                                                     # If the trajectory generation fails, break the loop
+            print(color_text("The trajectory is infeasible.", 'yellow'))
+            if pars.syntax_checker_enabled and syntax_checker_iteration <= pars.syntax_check_limit: # If the syntax checker is enabled
+                print(color_text("Checking the syntax of the specification...", 'yellow'))          # Check the syntax of the specification
+                syntax_checked_spec = translator.gpt_syntax_checker(spec)
+                syntax_checker_iteration += 1
 
-                elif syntax_checker_iteration > pars.syntax_check_limit:
-                    print(color_text("The program is terminated.", 'yellow'), "Exceeded the maximum number of syntax check iterations.")
-                    break
+            elif syntax_checker_iteration > pars.syntax_check_limit:
+                print(color_text("The program is terminated.", 'yellow'), "Exceeded the maximum number of syntax check iterations.")
+                break
 
 
         previous_messages = messages
